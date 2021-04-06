00ff9de91aa4c851a6f62eab4810dbff791601a2
dioyong <dioyong@naver.com>
2021-04-06 15:47:23 +0900

    App.js

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
-import MainPage from './pages/MainPage'
-import AboutPage from './pages/AboutPage'
+// import MainPage from './pages/MainPage'
+// import AboutPage from './pages/AboutPage'
+// import DetailPage from './pages/DetailPage'
+import { StatusBar } from 'expo-status-bar'
+import { NavigationContainer } from '@react-navigation/native'
+import StackNavigator from './navigation/StackNavigator'
 
 export default function App() {
+  return (
   // return (<MainPage/>);
-  return (<AboutPage/>);
+  // return (<AboutPage/>);
+  // return (<DetailPage/>);
+  <NavigationContainer>
+    <StatusBar style='black'/>
+    <StackNavigator/>
+  </NavigationContainer>
+  )
 }
 
 
