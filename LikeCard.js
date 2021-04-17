97fa5b9223e1f5ee1127de83f2c6cdd22f3ca272
dioyong <dioyong@naver.com>
2021-04-17 17:08:44 +0900

    커밋설명글

--- LikeCard.js
+++ LikeCard.js
@@ -1,22 +1,63 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+import Constants from 'expo-constants';
+import {firebase_db} from "../firebaseConfig";
 export default function LikeCard({content, navigation}) {
+
+  const detail = () => {
+    navigation.navigate('DetailPage', {idx:content.idx})
+  }
+
+  const remove = () => {
+    const user_id = Constants.installationId;
+
+    firebase_db.ref('/like/'+user_id+'/'+content.idx).remove().then(function(){
+        Alert.alert("삭제 완료");
+        // navigation.navigate('LikePage')
+        reload()
+    })
+  }
+
+
     return (      
-    //   <TouchableOpacity style={styles.card} onPress={()=>{navigation.navigate('DetailPage',content)}}>
-    <View>
+      <TouchableOpacity style={styles.card} onPress={()=>{navigation.navigate('DetailPage',{idx:content.idx})}}>
         <Image style={styles.cardImage} source={{uri:content.image}}/> 
         <View style={styles.cardText}>
           <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
           <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
           <Text style={styles.cardDate}>{content.date}</Text>
-       </View>
-    {/* </TouchableOpacity> */}
-    </View>
+         
+          <View style={styles.buttonGroup}>
+            <TouchableOpacity style={styles.button} onPress={() => detail()}>
+              <Text style={styles.buttonText}>자세히보기</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => remove()}>
+              <Text style={styles.buttonText}>찜해제</Text>
+            </TouchableOpacity>
+          </View>
+       </View>  
+     </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
+  buttonGroup: {
+    flexDirection: "row",
+  },
+  button: {
+    width: 90,
+    marginTop: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'deeppink',
+    borderRadius: 7
+  },
+  buttonText: {
+    color: 'deeppink',
+    textAlign: 'center'
+  },
     card:{
         flex:1,
         //컨텐츠들을 가로로 나열
