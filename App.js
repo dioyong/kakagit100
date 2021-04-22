5faabce1b56ed2047a2a493c51129fcf66b6193e
dioyong <dioyong@naver.com>
2021-04-22 21:46:26 +0900

    다시 복습 시작

--- App.js
+++ App.js
@@ -1,28 +1,21 @@
-import React from 'react'
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
-// import MainPage from './pages/MainPage'
-// import AboutPage from './pages/AboutPage'
-// import DetailPage from './pages/DetailPage'
-import { StatusBar } from 'expo-status-bar'
-import { NavigationContainer } from '@react-navigation/native'
-import StackNavigator from './navigation/StackNavigator'
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-  // return (<MainPage/>);
-  // return (<AboutPage/>);
-  // return (<DetailPage/>);
-  <NavigationContainer>
-    <StatusBar style='black'/>
-    <StackNavigator/>
-  </NavigationContainer>
-  )
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
-    //앱의 배경 색
+    flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
