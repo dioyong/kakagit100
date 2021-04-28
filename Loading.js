4f42f26ae87a5a15fbe4b046e62674cc16d90672
dioyong <dioyong@naver.com>
2021-04-28 23:07:19 +0900

    api 별로 페이지 분류

--- Loading.js
+++ Loading.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native';
-export default function Loading() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>준비중입니다...</Text>
-        </View>    
-        )
-    }        
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
 
-    const styles = StyleSheet.create({
-        container: {
+export default function Loading(){
+    return(<View style={styles.container}><Text style={styles.title}>준비중입니다...</Text></View>)
+}
+
+
+const styles = StyleSheet.create({
+    container: {
         //앱의 배경 색
         flex:1,
         justifyContent:'center',
         alignItems:'center',
         backgroundColor: '#fdc453',
-        },
-        title: {
+    },
+    title: {
         fontSize:20,
         fontWeight:'700'
-        }
-        })
-            
+    }
+
+})
