15f51097c3b2fdf6f3635ebdc46b70ed96f18a55
dioyong <dioyong@naver.com>
2021-05-01 19:22:01 +0900

    네비게이션 적용

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import OpenWeather from './pages/OpenWeather'
+// import OpenWeather from './pages/OpenWeather';
+// import PublicWeather from './pages/PublicWeather';
+import { StatusBar } from 'expo-status-bar';
+
+import {NavigationContainer, navigationContainer} from '@react-navigation/native';
+import StackNavigator from './navigation/StackNavigator';
+
 
 export default function App() {
-  return ( <OpenWeather/> ) 
+
+  console.disableYellowBox = true;
+
+  return ( 
+    <NavigationContainer> 
+      <StatusBar style='black' />
+      <StackNavigator/>
+    </NavigationContainer>
+  // <OpenWeather/> 
+  
+  );
 }
